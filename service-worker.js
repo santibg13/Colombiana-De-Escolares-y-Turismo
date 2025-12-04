1ea81583a41f98a5b259ba25ff5c45a27aa90783
santibg13 <santibg13@gmail.com>
2025-12-04 13:05:41 -0500

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,20 @@
-self.addEventListener("install", () => {
-    console.log("Service Worker instalado");
+self.addEventListener("install", (e) => {
+  e.waitUntil(
+    caches.open("colestur-cache").then((cache) => {
+      return cache.addAll([
+        "/Colombiana-De-Escolares-y-Turismo/index.html",
+        "/Colombiana-De-Escolares-y-Turismo/reservar.html",
+        "/Colombiana-De-Escolares-y-Turismo/contacto.html",
+        "/Colombiana-De-Escolares-y-Turismo/style.css"
+      ]);
+    })
+  );
 });
 
-self.addEventListener("fetch", (event) => {
-    event.respondWith(fetch(event.request));
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
+  );
 });
